566607b214f86fb08145585422900e96dc5cfe23
wassa1145 <sass_is@sgmk-group.ru>
2022-04-16 19:12:31 +0700

    --amend

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from './App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<App tab="home" />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App tab="home" />
+  </React.StrictMode>
 );
